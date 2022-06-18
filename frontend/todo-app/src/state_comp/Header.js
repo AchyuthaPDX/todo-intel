d9adec1603cc1f855235571fb12340646c82801e
Achyutha Harish <aharish@pdx.edu>
2022-06-17 19:08:26 -0700

    Adding few components and removing unreq files

---
+++ frontend/todo-app/src/state_comp/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="card bg-info text-center text-light rounded-0">
+        <h1 className="display-4">
+         <i className="fa fa-calendar-minus-o mr-3"></i><span className ="text-dark mr-3">Don't Forget</span> List
+        </h1>
+
+    </div>
+  )
+}
