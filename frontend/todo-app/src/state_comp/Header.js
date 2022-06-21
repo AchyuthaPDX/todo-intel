fcdb797b75807654d968983818b11bdb062bbd12
Achyutha Harish <aharish@pdx.edu>
2022-06-21 11:49:27 -0700

    Final touch to first part

--- frontend/todo-app/src/state_comp/Header.js
+++ frontend/todo-app/src/state_comp/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
+
 export default function Header() {
   return (
-    <div className="card bg-info text-center text-light rounded-0">
+    <div>
+      <div className="card-header text-center rounded-0">
         <h1 className="display-4">
-         <i className="fa fa-calendar-minus-o mr-3"></i> <span className ="text-dark mr-3"> Don't Forget </span> List
+         <i className="fa fa-calendar-minus-o mr-3"></i> <span className ="text-dark mr-3"> Don't Forget List</span>
         </h1>
-
+      </div>
+      
     </div>
   )
 }
